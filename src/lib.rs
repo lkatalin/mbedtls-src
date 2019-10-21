8b39e6f2674a5febaac69d0f3faaab93bb88fbc1
Lily S <lsturman@redhat.com>
2019-10-21 16:27:52 -0400

    testing config pull in from sys

--- src/lib.rs
+++ src/lib.rs
@@ -189,10 +189,11 @@ impl Build {
     fn generate_config(&self, inner_dir: &PathBuf) {
 	println!("GENERATE CONFIG WAS CALLED *****************\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         let curr = &env::var("CARGO_MANIFEST_DIR").unwrap();
+	println!("CARGO_MANIFEST_DIR is {}", curr);
         let root_dir = Path::new(curr).parent().unwrap();
-        let file = root_dir.join("src").join("config.h");
-        let _ = fs::copy(&file, inner_dir.join("include/mbedtls/config.h")).unwrap();
-        let _ = fs::copy(&file, &inner_dir.join("crypto/include/mbedtls/config.h")).unwrap();
+        //let file = root_dir.join("src").join("config.h");
+        //let _ = fs::copy(&file, inner_dir.join("include/mbedtls/config.h")).unwrap();
+        //let _ = fs::copy(&file, &inner_dir.join("crypto/include/mbedtls/config.h")).unwrap();
     }
 
     fn run_command(&self, mut command: Command, desc: &str) {
